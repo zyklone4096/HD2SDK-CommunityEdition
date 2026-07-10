373afebbf878a0c85312faae50a43ea015d3a1a7
Zyklone <me@zyklone.dev>
2026-07-10 22:32:59 +0800

    chore: version checker

--- __init__.py
+++ __init__.py
@@ -4792,7 +4792,7 @@ class HellDivers2ToolsPanel(Panel):
         if not OnCorrectBlenderVersion:
             row.label(text="Using Incorrect Blender Version!")
             row = layout.row()
-            row.label(text="Please Use Blender 4.0.X to 4.3.X")
+            row.label(text="Please Use Blender 5.1.X")
             return
 
 
